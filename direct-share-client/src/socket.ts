3845a509faab310ec331523912b2a0a1668b10ca
Santosh Nachan <51072075+SantoshN7@users.noreply.github.com>
2025-11-23 15:43:15 +0530

    socket secure

--- direct-share-client/src/socket.ts
+++ direct-share-client/src/socket.ts
@@ -2,6 +2,8 @@ import { io, Socket } from 'socket.io-client';
 
 const clientSocket: Socket = io('http://localhost:3000',  {
   withCredentials: true,
+  secure: true,
+  rejectUnauthorized: true,
   transports: ["websocket", "polling"] // Force fallback if needed
 }); // your server URL
 
